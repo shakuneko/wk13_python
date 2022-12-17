561323586e7e0b08eea99054d277e76cc3ae66f0
shakuneko <shakuneko47@gmail.com>
2022-12-17 17:03:27 +0800

    feed

--- db/articles_feed.py
+++ db/articles_feed.py
@@ -1,82 +1,41 @@
 articles = [
     {
-        "title": "11",
-        "author": "k",
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
-                            "has been the industry's standard dummy text ever since the 1500s, when an unknown "
-                            "printer took a galley of type and scrambled it to make a type specimen book. It has "
-                            "survived not only five centuries, but also the leap into electronic typesetting, "
-                            "remaining essentially unchanged. It was popularised in the 1960s with the release of "
-                            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
-                            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-
-        "description_long": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
-                            "has been the industry's standard dummy text ever since the 1500s, when an unknown "
-                            "printer took a galley of type and scrambled it to make a type specimen book. It has "
-                            "survived not only five centuries, but also the leap into electronic typesetting, "
-                            "remaining essentially unchanged. It was popularised in the 1960s with the release of "
-                            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
-                            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "image": "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        "title": "Hiphop Team",
+        "author": "S.COUPS",
+        "description": "為男子流行音樂組合「SEVENTEEN」和組合粉絲「CARAT」（캐럿）的隊長，同時也是分隊「Hip-hop Team」的隊長。崔勝哲以大眼睛、高鼻梁和漫畫人物般乾淨的臉型為特徵，而一張充滿異國風情的臉龐，常被誤會是混血兒。作為RAP主力成員之一，以舞台反轉魅力而出名，以出色的表現力，吸引了大家視線。作為SEVENTEEN總隊長，崔勝哲在成員們作為練習生開始便加以照顧，凡事都以成員為優先考慮，是位出名以充滿威嚴氣場而受到後輩們注目，充滿才華、責任感和領導能力的隊長。",
+        "description_long": "為男子流行音樂組合「SEVENTEEN」和組合粉絲「CARAT」（캐럿）的隊長，同時也是分隊「Hip-hop Team」的隊長。"
+                            "崔勝哲以大眼睛、高鼻梁和漫畫人物般乾淨的臉型為特徵，而一張充滿異國風情的臉龐，常被誤會是混血兒。 "
+                            "作為RAP主力成員之一，以舞台反轉魅力而出名，以出色的表現力，吸引了大家視線。作為SEVENTEEN總隊長， "
+                            "崔勝哲在成員們作為練習生開始便加以照顧，凡事都以成員為優先考慮，是位出名以充滿威嚴氣場而受到後輩們注目，"
+                            "充滿才華、責任感和領導能力的隊長。",
+                
+        "image": "https://imgur.com/4bmhc4R"
     },
     {
-        "title": "Article2",
-        "author": "Edwin",
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
-                            "has been the industry's standard dummy text ever since the 1500s, when an unknown "
-                            "printer took a galley of type and scrambled it to make a type specimen book. It has "
-                            "survived not only five centuries, but also the leap into electronic typesetting, "
-                            "remaining essentially unchanged. It was popularised in the 1960s with the release of "
-                            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
-                            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "title": "Vocal Team",
+        "author": "Jeonghan",
+        "description": "1995年10月4日出生於韓國首爾特別市，家庭成員有父母以及一位小四歲的妹妹。高中時為理科生，參與學校的戲劇社，就讀時受同學邀請而一同於演技學院學習。於高二期末前往演技學院途中，在地鐵站受到星探邀約，而後參加面試合格入選，成為Pledis娛樂練習生。高中畢業後進入東亞放送藝術大學廣告製作學系就讀，現已休學。2020年3月10日進入漢陽大學未來人才教育院，就讀實用音樂課程(K-POP)。2022年2月15日進入安養大學，就讀經營學系碩博士統合課程。",
 
-        "description_long": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
-                            "has been the industry's standard dummy text ever since the 1500s, when an unknown "
-                            "printer took a galley of type and scrambled it to make a type specimen book. It has "
-                            "survived not only five centuries, but also the leap into electronic typesetting, "
-                            "remaining essentially unchanged. It was popularised in the 1960s with the release of "
-                            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
-                            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "image": "https://images.pexels.com/photos/7026467/pexels-photo-7026467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        "description_long": "1995年10月4日出生於韓國首爾特別市，家庭成員有父母以及一位小四歲的妹妹。高中時為理科生，。"
+                            "參與學校的戲劇社，就讀時受同學邀請而一同於演技學院學習。於高二期末前往演技學院途中，"
+                            "在地鐵站受到星探邀約，而後參加面試合格入選，成為Pledis娛樂練習生。高中畢業後進入東亞"
+                            "放送藝術大學廣告製作學系就讀，現已休學。2020年3月10日進入漢陽大學未來人才教育院， "
+                            "就讀實用音樂課程(K-POP)。2022年2月15日進入安養大學，就讀經營學系碩博士統合課程。",
+        "image": "https://imgur.com/PBsyvbr"
     },
     {
-        "title": "Article3",
-        "author": "Nick",
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
-                            "has been the industry's standard dummy text ever since the 1500s, when an unknown "
-                            "printer took a galley of type and scrambled it to make a type specimen book. It has "
-                            "survived not only five centuries, but also the leap into electronic typesetting, "
-                            "remaining essentially unchanged. It was popularised in the 1960s with the release of "
-                            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
-                            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "title": "Performance Team",
+        "author": "Hoshi",
+        "description": "小時候為跆拳道選手，段位為黑帶4段。在配合音樂練習跆拳道品勢時對跳舞產生興趣，因此結合了跆拳道動作開始學習跳舞，由於爸爸和泰民的姑丈是好友，因此收到SHINee的專輯，進而產生當歌手的想法。同時權順榮多次提及自己為SHINee的粉絲。國中時成立舞蹈社團並參加多場比賽，在某次市內跳舞比賽獲得第一名因而獲得試鏡機會，通過試鏡後正式成為Pledis娛樂練習生。高中就讀磨石高等學校並於2015年2月畢業，由於正逢出道忙碌期間而推遲大學入學，後於2017年2月就讀東亞放送藝術大學校放送演藝學系。",
 
-        "description_long": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
-                            "has been the industry's standard dummy text ever since the 1500s, when an unknown "
-                            "printer took a galley of type and scrambled it to make a type specimen book. It has "
-                            "survived not only five centuries, but also the leap into electronic typesetting, "
-                            "remaining essentially unchanged. It was popularised in the 1960s with the release of "
-                            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
-                            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "image": "https://images.pexels.com/photos/8110956/pexels-photo-8110956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        "description_long": "小時候為跆拳道選手，段位為黑帶4段。在配合音樂練習跆拳道品勢時對跳舞產生興趣，"
+                            "因此結合了跆拳道動作開始學習跳舞，由於爸爸和泰民的姑丈是好友，因此收到SHINee的專輯，"
+                            "進而產生當歌手的想法。同時權順榮多次提及自己為SHINee的粉絲。國中時成立舞蹈社團並參加"
+                            "多場比賽，在某次市內跳舞比賽獲得第一名因而獲得試鏡機會，通過試鏡後正式成為Pledis娛樂"
+                            "練習生。高中就讀磨石高等學校並於2015年2月畢業，由於正逢出道忙碌期間而推遲大學入學，"
+                            "後於2017年2月就讀東亞放送藝術大學校放送演藝學系。",
+                            
+        "image": "https://imgur.com/l6vdf1v"
     },
-    {
-        "title": "Article4",
-        "author": "claire",
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
-                            "has been the industry's standard dummy text ever since the 1500s, when an unknown "
-                            "printer took a galley of type and scrambled it to make a type specimen book. It has "
-                            "survived not only five centuries, but also the leap into electronic typesetting, "
-                            "remaining essentially unchanged. It was popularised in the 1960s with the release of "
-                            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
-                            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-
-        "description_long": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
-                            "has been the industry's standard dummy text ever since the 1500s, when an unknown "
-                            "printer took a galley of type and scrambled it to make a type specimen book. It has "
-                            "survived not only five centuries, but also the leap into electronic typesetting, "
-                            "remaining essentially unchanged. It was popularised in the 1960s with the release of "
-                            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
-                            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "image": "https://www.pexels.com/zh-tw/photo/cgi-12843338/"
-    }
+  
 ]
