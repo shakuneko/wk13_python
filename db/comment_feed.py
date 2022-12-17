2b74f797d10a62863b521b8284cc66be95b0f088
shakuneko <shakuneko47@gmail.com>
2022-12-17 23:40:53 +0800

    feed

--- db/comment_feed.py
+++ db/comment_feed.py
@@ -1,69 +1,38 @@
 comment = [
     {
-        "content": "say the name!",
+        "content": "韓國Pledis娛樂旗下的13人男子流行音樂團體，團體有Hiphop Team、Vocal Team、Performance Team三個分隊",
         "article_id": 1,
-        "user_id": 3
-    },
-    {
-        "content": "호랑해~~",
-        "article_id": 2,
-        "user_id": 5
-    },
-    {
-        "content": "carat ♥ ♥",
-        "article_id": 2,
         "user_id": 1
     },
-    {
-        "content": "seventeen ♥",
-        "article_id": 3,
+       {
+        "content": "團體名字「SEVENTEEN」寓意為「13名成員+3個分隊+1個整體」，問候語(打招呼口號)為「Say the name, SEVENTEEN」，同時左手比1，右手比7，並將兩手向前伸再彎曲交疊。",
+        "article_id": 1,
         "user_id": 2
     },
     {
-        "content": "하니해~~~~",
-        "article_id": 3,
-        "user_id": 4
-    },
-    {
-        "content": "쿱랑해~~~~",
-        "article_id": 3,
+        "content": "韓國Pledis娛樂旗下的13人男子流行音樂團體，團體有Hiphop Team、Vocal Team、Performance Team三個分隊",
+        "article_id": 2,
         "user_id": 3
     },
     {
-        "content": "슈아해~~",
-        "article_id": 4,
-        "user_id": 1
-    },
-    {
-        "content": "준랑해~~~",
-        "article_id": 4,
+        "content": "公開官方粉絲名稱「CARAT（캐럿）」取自於鑽石的質量單位，比喻SEVENTEEN是像鑽石一樣閃耀的存在，隨著CARAT的數量增加SEVENTEEN的價值也越高。",
+        "article_id": 2,
         "user_id": 4
     },
     {
-        "content": "원랑해~~~~",
-        "article_id": 5,
-        "user_id": 1
-    },
-    {
-        "content": "우아해 ς(>‿<.)",
-        "article_id": 5,
-        "user_id": 2
-    },
-    {
-        "content": "규랑해 ς( ᐡ-ﻌ•ᐡ )",
-        "article_id": 5,
-        "user_id": 3
-    },
-    {
-        "content": "팔랑해 ( ღ⸝⸝¯ ⁸ ¯⸝⸝ )",
-        "article_id": 5,
-        "user_id": 4
-    },
-    {
-        "content": "도아해 (｡•̀ᴗ-ღ)",
-        "article_id": 5,
+        "content": "韓國Pledis娛樂旗下的13人男子流行音樂團體，團體有Hiphop Team、Vocal Team、Performance Team三個分隊",
+        "article_id": 3,
         "user_id": 5
-    }
+    },
+    {
+        "content": "公開官方應援色為玫瑰石英粉與寧靜藍。玫瑰石英粉是暖和且柔軟的，而寧靜藍則是沉靜、平穩的，用來描述SEVENTEEN的溫暖及清澈。",
+        "article_id": 3,
+        "user_id": 1
+    },
+  
+   
+ 
+   
 
 
 
--- db/users_feed.py
+++ db/users_feed.py
@@ -2,26 +2,21 @@ users = [
     {
         "username": "Joshua",
         "avatar":"https://raw.githubusercontent.com/shakuneko/pic0412/main/pic3.jpg",
-        "email":"Joshua_1230@gamil.com"
     },
     {
         "username": "Jun",
         "avatar": "https://raw.githubusercontent.com/shakuneko/pic0412/main/pic4.jpg",
-        "email": "Jun_0610@gamil.com"
     },
     {
         "username": "Wonwoo",
         "avatar": "https://raw.githubusercontent.com/shakuneko/pic0412/main/pic6.jpg",
-        "email": "Wonwoo_0717@gamil.com"
     },
     {
         "username": "Woozi",
         "avatar": "https://raw.githubusercontent.com/shakuneko/pic0412/main/pic7.jpg",
-        "email": "Woozi_1122@gamil.com"
     },
     {
         "username": "THE 8",
         "avatar": "https://raw.githubusercontent.com/shakuneko/pic0412/main/pic8.JPG",
-        "email": "THE 8_1107@gamil.com"
     },
 ]
